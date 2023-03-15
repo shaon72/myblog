261064d4185b47ddc73763b528e8dcbfcbe3ef72
shaon72 <shaondebnath72@gmail.com>
2023-03-15 15:49:42 +0530

    modified path

--- components/Navbar.js
+++ components/Navbar.js
@@ -56,14 +56,12 @@ const Navbar = () => {
             <div className="-my-6 divide-y divide-gray-500/10">
               <div className="space-y-2 py-6">
                 
-                <a
-                  href="#"
+                <a href="#"
                   className="-mx-3 block rounded-lg py-2 px-3 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                 >
                   Blog
                 </a>
-                <a
-                  href="/myblog/about-me"
+                <a href="/myblog/about-me"
                   className="-mx-3 block rounded-lg py-2 px-3 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                 >
                   About me
